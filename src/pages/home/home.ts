1fad68c1becc5f3020b972683083383f4b717291
Vidhan Jhawar (Residents) <vidhan.jhawar98@gmail.com>
2018-09-17 19:03:47 +0530

    Implemented Google Autocompleter using custom address suggestion box

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, NgZone,ViewChild,ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MapsAPILoader } from '@agm/core';
 
+declare var google: any;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public zone: NgZone,private mapsAPILoader: MapsAPILoader) {
+    this.GoogleAutocomplete = new google.maps.places.AutocompleteService();
+    this.autocomplete = { input: '' };
+    this.autocompleteItems = [];
   }
 
+  updateSearchResults (){
+    if (this.autocomplete.input == '') {
+      this.autocompleteItems = [];
+      return;
+    }
+    this.GoogleAutocomplete.getPlacePredictions({ input: this.autocomplete.input },
+  	(predictions, status) => {
+      this.autocompleteItems = [];
+      if(predictions) {
+        this.zone.run(() => {
+          predictions.forEach((prediction) => {
+            this.autocompleteItems.push(prediction);
+            });
+        });
+      }
+    });
+  }
 }
